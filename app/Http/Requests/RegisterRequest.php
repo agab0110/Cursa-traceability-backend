e2d92326438ee5eced1084c1dad9b8f405836de2
agab0110 <agabrielex01@gmail.com>
2023-12-20 16:40:25 +0100

    created register request

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'cf' => 'required|unique:users,cf|string|max:16|min:16',
+            'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'birth_date' => 'required',
+            'email' => 'required',
+            'role' => 'required',
+        ];
+    }
+}
