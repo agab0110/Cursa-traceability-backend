7014208afedff6b7045ac6769a0f8698e4dc07f7
agab0110 <agabrielex01@gmail.com>
2023-12-20 16:46:47 +0100

    added update user function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\UpdatetUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function update(UpdatetUserRequest $request, User $user) {
+        $validated = $request->validated();
+
+        $user->update($validated);
+
+        return response()->json([
+            'message' => 'Utente aggiornato con successo',
+            'data' => $user,
+        ], 200);
+    }
 }
