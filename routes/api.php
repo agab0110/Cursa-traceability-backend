e85426bb480e3898630efd8ac3f882432e4547b3
agab0110 <agabrielex01@gmail.com>
2023-12-20 16:51:53 +0100

    added user controller route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::post('logout', [UserController::class, 'logout']);
 });
 
 Route::prefix('auth')->group(function () {
