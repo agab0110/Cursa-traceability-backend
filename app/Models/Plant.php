fa4598743567b0080aa81431410e8e5d38b7a87e
agab0110 <agabrielex01@gmail.com>
2023-12-20 16:32:40 +0100

    created plant model

---
+++ app/Models/Plant.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [     // TODO: aggiungere tag NFC
+        'lat',
+        'lng',
+        'woods',
+        'particle',
+        'woody_species',
+        'diameter',
+        'height',
+        'cultivar',
+        'propagation',
+        'georeferenzial_date',
+        'notes',
+        'hammered'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'georeferenzial_date' => 'datetime',
+        'hammered' => 'boolean'
+    ];
+}
