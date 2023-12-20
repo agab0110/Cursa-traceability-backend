3ec45ec6d8778403c8c8233a878e4f933922bde3
agab0110 <agabrielex01@gmail.com>
2023-12-20 16:39:46 +0100

    added login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request) {
+        $validated = $request->validated();
+
+        if (!Auth::attempt($validated)) {
+            return response()->json([
+                'message' => 'Parametri non validi',
+            ], 401);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer'
+        ]);
+    }
 }
