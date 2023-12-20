de93025e241e47b0e3ca02f9464a9107e8bc4e05
agab0110 <agabrielex01@gmail.com>
2023-12-20 16:30:54 +0100

    created plants table migration

---
+++ database/migrations/2023_12_20_153025_create_plants_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->double('lat');
+            $table->double('lng');
+            $table->string('woods');
+            $table->integer('particle')->nullable();
+            $table->string('woody_species');
+            $table->double('diameter');
+            $table->double('height')->nullable();
+            $table->string('cultivar')->nullable();
+            $table->string('propagation')->nullable();
+            $table->date('georeferenzial_date');
+            $table->string('notes')->nullable();
+            $table->boolean('hammered')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plants');
+    }
+};
