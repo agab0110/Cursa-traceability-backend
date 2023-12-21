97eeb4208d89b6091570a1c21508942da495358d
agab0110 <agabrielex01@gmail.com>
2023-12-21 09:54:24 +0100

    added hashed cast

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password')->nullable()->hash();
             $table->rememberToken();
             $table->timestamps();
         });
