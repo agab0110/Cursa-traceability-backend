554caf207dc52f348709ba991646bf4112974803
agab0110 <agabrielex01@gmail.com>
2023-12-21 09:54:54 +0100

    removed hash generation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable()->hash();
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
