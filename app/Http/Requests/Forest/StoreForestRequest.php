b892efca6d0106df6949f4212944cdd197fe8eda
agab0110 <agabrielex01@gmail.com>
2023-12-21 12:29:51 +0100

    created store forest request

---
+++ app/Http/Requests/Forest/StoreForestRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Forest;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreForestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'region' => 'required|string|max:255'
+        ];
+    }
+}
