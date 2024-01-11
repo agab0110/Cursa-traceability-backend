f47281f63de44250d38f5b0669598271eee4ac0f
agab0110 <agabrielex01@gmail.com>
2024-01-11 16:06:25 +0100

    removed logout function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\User\UpdateUserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -18,12 +17,4 @@ class UserController extends Controller
             'data' => $user,
         ], 200);
     }
-
-    public function logout(Request $request) {
-        $request->user()->token()->revoke();
-
-         return response()->json([
-             'message' => 'Logout effettuato con successo'
-         ], 200);
-     }
 }
