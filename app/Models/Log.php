8d17d573608090649f90d9baa8277d9f2de6a912
agab0110 <agabrielex01@gmail.com>
2024-01-14 16:55:11 +0100

    added foreign key on plant

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Log extends Model
 {
@@ -22,4 +23,8 @@ class Log extends Model
     protected $casts = [
         'cut_date' => 'datetime',
     ];
+
+    public function plant(): BelongsTo {
+        return $this->belongsTo(Plant::class);
+    }
 }
