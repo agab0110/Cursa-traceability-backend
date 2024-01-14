89e0fc87c143644f306b81c53a89d2794c3e7a56
agab0110 <agabrielex01@gmail.com>
2024-01-14 12:36:50 +0100

    created lot controller

---
+++ app/Http/Controllers/LotController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lot;
+use Illuminate\Http\Request;
+
+class LotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $lots = Lot::all()->paginate(13);
+
+        if (!$lots) {
+            return response()->json([
+                'message' => 'Lotti non trovati',
+            ], 404);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
