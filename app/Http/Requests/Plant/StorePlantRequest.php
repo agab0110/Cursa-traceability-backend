332e4775682250fb8c4cbae7b8cca19c7564c99e
agab0110 <agabrielex01@gmail.com>
2024-01-14 16:06:50 +0100

    added hammered date

--- app/Http/Requests/Plant/StorePlantRequest.php
+++ app/Http/Requests/Plant/StorePlantRequest.php
@@ -32,7 +32,7 @@ class StorePlantRequest extends FormRequest
             'height' => 'sometimes|nullable|numeric',
             'cultivar' => 'sometimes|nullable|string|max:255',
             'propagation' => 'required|string|max:255',
-            'georeferenzial_date' => 'required',
+            'hammered_date' => 'required',
             'notes' => 'nullable|sometimes|string|max:255',
             'hammered' => 'sometimes|required|boolean',
             'forest_id' => 'required|numeric'
