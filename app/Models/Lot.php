2cfec12b12ffc17ae21ece89bbeb2f3a307b15ba
agab0110 <agabrielex01@gmail.com>
2024-01-14 12:37:00 +0100

    created lot model

---
+++ app/Models/Lot.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Lot extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'plant_id'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function plant(): HasOne {
+        return $this->hasOne(Plant::class);
+    }
+}
