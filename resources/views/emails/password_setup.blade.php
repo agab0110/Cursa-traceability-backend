9c641e351accd5f350d91352e41a65792179d01e
agab0110 <agabrielex01@gmail.com>
2024-05-11 19:03:22 +0200

    fixed grafic

--- resources/views/emails/password_setup.blade.php
+++ resources/views/emails/password_setup.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Setup</title>
-</head>
-<body>
-    <p>Hello,</p>
-    <p>Please click the following link to set up your password:</p>
-    <a href="{{ route('password.setup') }}?token={{ $token }}">Set Up Password</a>
-    <p>If you didn't request this, you can ignore this email.</p>
-</body>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Setup</title>
+        <link rel="stylesheet" href="{{ asset('mailstyle.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <p>Gentile utente,</p>
+            <p>Si prega di cliccare sul seguente link per inserire una nuova password per il vostro account:</p>
+            <p>
+                <a href="{{ route('password.setup') }}?token={{ $token }}">Reset password</a>
+            </p>
+            <p>Se questa mail non è stata richiesta si prega di ignorarla.</p>
+        </div>
+    </body>
 </html>
