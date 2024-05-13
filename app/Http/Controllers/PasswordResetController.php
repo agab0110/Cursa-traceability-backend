2e68149b214d7bddcff40bc30c53be26a46a276a
agab0110 <agabrielex01@gmail.com>
2024-05-13 10:09:02 +0200

    added return view to password success

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -83,6 +83,7 @@ class PasswordResetController extends Controller
         // Delete the token from password resets table
         DB::table('password_resets')->where('email', $user->email)->delete();
 
-        return response()->json(['message' => 'La passoword è stata aggiornata con successo'], 200);
+        //return response()->json(['message' => 'La passoword è stata aggiornata con successo'], 200);
+        return view('password_success');
     }
 }
