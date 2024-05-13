e86dc0c59b03cbc34005eb52b13818bee4d732fa
agab0110 <agabrielex01@gmail.com>
2024-05-13 10:08:36 +0200

    added head components

--- resources/views/password_success.blade.php
+++ resources/views/password_success.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="it">
     <head>
-
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Success</title>
+        <link rel="stylesheet" href="{{ asset('style.css') }}">
     </head>
     <body>
-        
+
     </body>
 </html>
