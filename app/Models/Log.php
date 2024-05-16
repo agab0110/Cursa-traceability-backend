cddff425b42ce977cba060c8355d54ba9cb700a7
agab0110 <agabrielex01@gmail.com>
2024-05-16 15:51:27 +0200

    added composite key

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -2,13 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Thiagoprz\CompositeKey\HasCompositeKey;
 
 class Log extends Model
 {
+    use HasCompositeKey;
+
+    protected $primaryKey = [
+        'number',
+        'lot_id'
+    ];
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
     protected $fillable = [
+        'number',
         'lot_id',
         'lenght',
         'median',
