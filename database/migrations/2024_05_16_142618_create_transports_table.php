4cd198033f23a46c79781fa17f408f6d2246ac62
agab0110 <agabrielex01@gmail.com>
2024-05-16 16:32:49 +0200

    removed timestamps

--- database/migrations/2024_05_16_142618_create_transports_table.php
+++ database/migrations/2024_05_16_142618_create_transports_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->date('shipping_date');
             $table->boolean('shipped')->default(false);
             $table->date('shipped_date')->nullable();
-            $table->timestamps();
         });
     }
 
