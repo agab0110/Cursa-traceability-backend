db33ccf1d67aeee36405829a841145fa53f7ed4c
agab0110 <agabrielex01@gmail.com>
2024-05-16 16:57:23 +0200

    added transport foreign key

--- app/Models/Sawmill.php
+++ app/Models/Sawmill.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sawmill extends Model
 {
     use HasFactory;
+
+    public function transports(): HasMany {
+        return $this->hasMany(Transport::class);
+    }
 }
