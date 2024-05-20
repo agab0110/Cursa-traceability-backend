5e72f45bc950b77a5ed3597497039328c7072d80
agab0110 <agabrielex01@gmail.com>
2024-05-20 10:48:53 +0200

    changed name to company name

--- database/migrations/2024_05_20_071502_create_pre_productions_table.php
+++ database/migrations/2024_05_20_071502_create_pre_productions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('pre_productions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('company_name');
             $table->timestamps();
         });
     }
