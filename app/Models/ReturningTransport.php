7c2b10c8329d660a96b47bc1cecbc65eba7c39d1
agab0110 <agabrielex01@gmail.com>
2024-05-24 18:03:41 +0200

    renamed file

--- app/Models/ReturningTransport.php
+++ app/Models/ReturningTransport.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class ReturningTransports extends Model
+class ReturningTransport extends Model
 {
     use HasFactory;
 
