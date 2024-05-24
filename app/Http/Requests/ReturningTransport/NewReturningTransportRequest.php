f99da01013c23a76e1e68dfe9875fd6fd8742594
agab0110 <agabrielex01@gmail.com>
2024-05-24 18:05:15 +0200

    added validation rules

--- app/Http/Requests/ReturningTransport/NewReturningTransportRequest.php
+++ app/Http/Requests/ReturningTransport/NewReturningTransportRequest.php
@@ -11,7 +11,7 @@ class NewReturningTransportRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class NewReturningTransportRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'transport_id' => 'required|integer',
+            'notes' => 'sometimes|string',
+            'returning_date' => 'required|date'
         ];
     }
 }
