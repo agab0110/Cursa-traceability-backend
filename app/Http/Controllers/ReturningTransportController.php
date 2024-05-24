c7a8d23bc80239f777a7b9b85517e21823a4f966
agab0110 <agabrielex01@gmail.com>
2024-05-24 18:09:38 +0200

    updated store function

--- app/Http/Controllers/ReturningTransportController.php
+++ app/Http/Controllers/ReturningTransportController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ReturningTransport\NewReturningTransportRequest;
+use App\Http\Responses\ApiResponse;
+use App\Models\ReturningTransport;
 use Illuminate\Http\Request;
 
 class ReturningTransportController extends Controller
@@ -15,11 +18,24 @@ class ReturningTransportController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created returning transport in storage.
+     *
+     * @param App\Http\Requests\ReturningTransport\NewReturningTransportRequest $request containing the required field
+     * @return App\Http\Responses\ApiResponse with the returning transport created
      */
-    public function store(Request $request)
+    public function store(NewReturningTransportRequest $request)
     {
-        //
+        $validated = $request->validated();
+
+        $returningTransport = new ReturningTransport();
+
+        $returningTransport->transport_id = $validated['transport_id'];
+        $returningTransport->notes = $validated['notes'];
+        $returningTransport->returing_date = $validated['returing_date'];
+
+        $returningTransport->save();
+
+        return new ApiResponse('Trasporto di ritorno creato con successo', $returningTransport, 201);
     }
 
     /**
