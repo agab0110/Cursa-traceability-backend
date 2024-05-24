8840ba425b134105ec5de76d8108e5d9912fca40
agab0110 <agabrielex01@gmail.com>
2024-05-24 17:37:16 +0200

    added validation rules

--- app/Http/Requests/PreProduction/CreateLogSectionRequest.php
+++ app/Http/Requests/PreProduction/CreateLogSectionRequest.php
@@ -11,7 +11,7 @@ class CreateLogSectionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class CreateLogSectionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'lot_id' => 'required|integer',
+            'log_number' => 'required|integer'
         ];
     }
 }
