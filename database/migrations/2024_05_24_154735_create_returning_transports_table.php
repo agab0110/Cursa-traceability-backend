cd5d16f7e861ee56f0940d9930f88da49c440e74
agab0110 <agabrielex01@gmail.com>
2024-05-24 17:50:10 +0200

    added columns

--- database/migrations/2024_05_24_154735_create_returning_transports_table.php
+++ database/migrations/2024_05_24_154735_create_returning_transports_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('returning_transports', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('transport_id')->constrained();
+            $table->string('notes');
+            $table->date('returning_date');
             $table->timestamps();
         });
     }
