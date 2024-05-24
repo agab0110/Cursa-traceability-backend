e95dddd756d8942d89f200086e93f82bb6c1df75
agab0110 <agabrielex01@gmail.com>
2024-05-24 17:54:20 +0200

    added foreign key to transport

--- app/Models/ReturningTransports.php
+++ app/Models/ReturningTransports.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReturningTransports extends Model
 {
@@ -15,4 +16,8 @@ class ReturningTransports extends Model
         'returing_date',
         'returned_date'
     ];
+
+    public function transport(): BelongsTo {
+        return $this->belongsTo(Transport::class);
+    }
 }
