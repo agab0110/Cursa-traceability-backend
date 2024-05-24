f46ddaf01e9d1ceac965e4683598b8c4836caa91
agab0110 <agabrielex01@gmail.com>
2024-05-24 15:47:06 +0200

    updated index function

--- app/Http/Controllers/TransportController.php
+++ app/Http/Controllers/TransportController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ApiException;
+use App\Http\Responses\ApiResponse;
+use App\Models\Transport;
 use Illuminate\Http\Request;
 
 class TransportController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the transport for a specific company.
+     *
+     * @param Illuminate\Http\Request $request containing the company name
+     * @return App\Http\Responses\ApiResponse with the list of transprts
+     * @throws App\Exceptions\ApiException with an error message if no transports are found
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $transports = Transport::where('company_name', $request->query('company_name'));
+
+        if (!$transports) {
+            throw new ApiException('Trasporti non trovati', 404);
+        }
+
+        return new ApiResponse('Trasporti trovati', $transports, 200);
     }
 
     /**
