818beb921cb9336d7995de9eb152cda22c728d04
agab0110 <agabrielex01@gmail.com>
2024-05-27 13:01:05 +0200

    fixed field

--- app/Http/Requests/PreProduction/UpdatePreProductionRequest.php
+++ app/Http/Requests/PreProduction/UpdatePreProductionRequest.php
@@ -22,7 +22,7 @@ class UpdatePreProductionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|min:3'
+            'company_name' => 'required|string|min:3'
         ];
     }
 }
