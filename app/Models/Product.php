87b9e1aa85e6e2388c327eda89e88262ec5f8c84
agab0110 <agabrielex01@gmail.com>
2024-05-27 10:05:19 +0200

    added production foreign key

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -20,4 +21,8 @@ class Product extends Model
         'created_at',
         'updated_at'
     ];
+
+    public function production(): BelongsTo {
+        return $this->belongsTo(Production::class);
+    }
 }
