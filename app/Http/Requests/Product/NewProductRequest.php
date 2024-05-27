63a0cf52705588bd7f1695c87343c2e0f571f7e3
agab0110 <agabrielex01@gmail.com>
2024-05-27 10:13:41 +0200

    added validation rules

--- app/Http/Requests/Product/NewProductRequest.php
+++ app/Http/Requests/Product/NewProductRequest.php
@@ -11,7 +11,7 @@ class NewProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class NewProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|min:3',
+            'production_id' => 'required|integer',
+            'log_number' => 'required|integer',
+            'lot_id' => 'required|integer'
         ];
     }
 }
