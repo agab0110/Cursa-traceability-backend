0771ac457d8643bf166a502abffaa4500822987b
agab0110 <agabrielex01@gmail.com>
2024-05-27 12:40:48 +0200

    updated index function

--- app/Http/Controllers/LogSectionController.php
+++ app/Http/Controllers/LogSectionController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ApiException;
+use App\Http\Responses\ApiResponse;
+use App\Models\LogSection;
 use Illuminate\Http\Request;
 
 class LogSectionController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @param Illuminate\Http\Request $request containing the lot id and the log number
+     * @return App\Http\Responses\ApiResponse with the sections found
+     * @throws App\Exceptions\ApiException with an error message if no section is found
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $sections = LogSection::where('lot_id', $request->lot_id)->where('log_number', $request->log_number);
+
+        if (!$sections) {
+            throw new ApiException('Nessuna sezione presente', 404);
+        }
+
+        return new ApiResponse('Sezioni trovate', $sections, 200);
     }
 
     /**
