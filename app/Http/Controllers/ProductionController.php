e7ae54ba04c365391f950f25bc132188a6e2414d
agab0110 <agabrielex01@gmail.com>
2024-05-27 09:40:22 +0200

    updated store function

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Production\NewProductionRequest;
+use App\Http\Responses\ApiResponse;
+use App\Models\Production;
 use Illuminate\Http\Request;
 
 class ProductionController extends Controller
@@ -15,11 +18,25 @@ class ProductionController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created production in storage.
+     *
+     * @param App\Http\Requests\Production\NewProductionRequest $request with the field required
+     * @return App\Http\Responses\ApiResponse with the created production
      */
-    public function store(Request $request)
+    public function store(NewProductionRequest $request)
     {
-        //
+        $validated = $request->validated();
+
+        $production = new Production();
+
+        $production->name = $validated['name'];
+        $production->pre_production_id = $validated['pre_production_id'];
+        $production->lot_id = $validated['lot_id'];
+        $production->log_number = $validated['log_number'];
+
+        $production->save();
+
+        return new ApiResponse('Produzione creata con successo', $production, 201);
     }
 
     /**
