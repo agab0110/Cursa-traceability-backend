54e7c56444eccf5e5bddb1ad702a1f906dfa2f42
agab0110 <agabrielex01@gmail.com>
2024-05-27 10:52:03 +0200

    added factory field

--- database/factories/LotFactory.php
+++ database/factories/LotFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Forest;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,7 @@ class LotFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'plant_id' => Forest::factory()
         ];
     }
 }
