e091c82d60f5f57b363e9e7b74891018c0ceb1e9
agab0110 <agabrielex01@gmail.com>
2024-06-20 11:42:28 +0200

    created estimation model model

---
+++ app/Models/EstimationModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EstimationModel extends Model
+{
+    use HasFactory;
+}
