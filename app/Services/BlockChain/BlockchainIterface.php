4533ccefcc95a55159bacd3fc2f7f6ea626fc8fc
agab0110 <agabrielex01@gmail.com>
2025-01-21 19:44:34 +0100

    changed class name

--- app/Services/BlockChain/BlockchainIterface.php
+++ app/Services/BlockChain/BlockchainIterface.php
@@ -2,7 +2,7 @@
 
 namespace App\Services\Blockchain;
 
-interface BlockchainImplementation
+interface BlockchainInterface
 {
     public function connect();
     public function sendTransaction(array $transactionData);
