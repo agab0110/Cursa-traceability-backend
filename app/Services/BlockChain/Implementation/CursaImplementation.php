109e0ba5244ef4c35822ed21a39e0f2c72825124
agab0110 <agabrielex01@gmail.com>
2025-02-04 13:49:29 +0100

    created cursa implementation

---
+++ app/Services/BlockChain/Implementation/CursaImplementation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Blockchain\Implementation;
+
+use App\Services\Blockchain\BlockchainInterface;
+
+use function PHPUnit\Framework\returnSelf;
+
+class CursaImplementation implements BlockchainInterface
+{
+    public function connect() {
+        return "Connesso";
+    }
+
+    public function getData(string $id) {
+        return "Get data success";
+    }
+
+    public function sendTransaction(array $data) {
+        return "Send data success";
+    }
+}
