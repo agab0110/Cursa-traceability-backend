5f91270877cf3fecbb7c9070d29e69fcabfca58d
agab0110 <agabrielex01@gmail.com>
2025-02-04 14:19:18 +0100

    added functions docblock

--- app/Services/BlockChain/Implementation/CursaImplementation.php
+++ app/Services/BlockChain/Implementation/CursaImplementation.php
@@ -9,14 +9,29 @@ use App\Services\Blockchain\BlockchainInterface;
  */
 class CursaImplementation implements BlockchainInterface
 {
+    /**
+     * Implementazione di connessione alla blockchain
+     */
     public function connect() {
         return "Connesso";
     }
 
+    /**
+     * Implementazione getData
+     *
+     * @param string $id l'id della transazione
+     * @return
+     */
     public function getData(string $id) {
         return "Get data success";
     }
 
+    /**
+     * Implementazione sendData
+     *
+     * @param array $data i dati da inviare
+     * @return
+     */
     public function sendTransaction(array $data) {
         return "Send data success";
     }
