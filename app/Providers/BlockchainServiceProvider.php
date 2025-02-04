f5e7a66d41cf9862e4971cf38f73925e70cf090e
agab0110 <agabrielex01@gmail.com>
2025-02-04 14:09:32 +0100

    added docblock

--- app/Providers/BlockchainServiceProvider.php
+++ app/Providers/BlockchainServiceProvider.php
@@ -6,6 +6,9 @@ use App\Services\Blockchain\BlockchainBridge;
 use App\Services\Blockchain\Implementation\CursaImplementation;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * In questa classe è possibile cambiare l'implementazione della blockchain a piacimento
+ */
 class BlockchainServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +16,8 @@ class BlockchainServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //$blockchainType = config('blockchain.default'); // Legge la configurazione, in base ad essa con un if si cambia l'implementazione
+        // Legge la configurazione, in base ad essa con un if si cambia l'implementazione
+        //$blockchainType = config('blockchain.default');
         $this->app->bind(BlockchainBridge::class, function($app) {
             return new BlockchainBridge(new CursaImplementation);
         });
