50f57ac6e14f91c8e90b5eb0489e4e6a4143ada7
agab0110 <agabrielex01@gmail.com>
2025-02-04 14:17:46 +0100

    added functions docblock

--- app/Services/BlockChain/BlockchainBridge.php
+++ app/Services/BlockChain/BlockchainBridge.php
@@ -10,20 +10,40 @@ use App\Services\Blockchain\BlockchainInterface;
 class BlockchainBridge{
     protected BlockchainInterface $blockchain;
 
+    /**
+     * Dependency injection
+     *
+     * @param App\Services\Blockchain\BlockchainInterface $blockchain
+     */
     public function __construct(BlockchainInterface $blockchain)
     {
         $this->blockchain = $blockchain;
     }
 
+    /**
+     * Funzione di connessione alla blockchain
+     */
     public function connect() {
         return $this->blockchain->connect();
     }
 
-    public function getBalance(string $id) {
+    /**
+     * Funzione per prendere i dati di una determinata transazione
+     *
+     * @param string $id l'id della transazione
+     * @return App\Services\Blockchain\BlockchainInterface la funzione getData() dell'interfaccia
+     */
+    public function getData(string $id) {
         return $this->blockchain->getData($id);
     }
 
-    public function sendTransaction(array $data) {
+    /**
+     * Funzione per inviare i dati alla blockchain
+     *
+     * @param array $data i dati da inviare
+     * @return App\Services\Blockchain\BlockchainInterface la funzione sendData() dell'interfaccia
+     */
+    public function sendData(array $data) {
         return $this->blockchain->sendTransaction($data);
     }
 }
