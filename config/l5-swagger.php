c93d5a8d9ab211a9d4f22822cd854f50c7854129
agab0110 <agabrielex01@gmail.com>
2025-02-18 20:31:35 +0100

    updated storage path

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -81,7 +81,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => storage_path('api-docs'),
+            'docs' => storage_path('docs/swagger/api'),
 
             /*
              * Absolute path to directory where to export views
