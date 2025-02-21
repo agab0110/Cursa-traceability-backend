63efcee0a32fe7d1497d7329235d2605040cc7dd
agab0110 <agabrielex01@gmail.com>
2025-02-21 15:03:50 +0100

    updated view page

--- resources/views/classDocs.blade.php
+++ resources/views/classDocs.blade.php
@@ -1,1 +1,11 @@
-<iframe src="{{ asset('public\docs\classes\index.html') }}" style="width: 100%; height: 100vh; border: none;"></iframe>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documentazione classi</title>
+</head>
+<body style="margin:0; padding:0;">
+    <iframe src="{{ asset('docs\classes\index.html') }}" width="100%" height="1000px" style="border:none;"></iframe>
+</body>
+</html>
