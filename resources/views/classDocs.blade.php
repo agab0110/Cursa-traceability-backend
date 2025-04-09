4f0af74d2a60d7307e44d34a393b8caa95fd7811
agab0110 <agabrielex01@gmail.com>
2025-04-09 15:28:57 +0200

    changed indentation

--- resources/views/classDocs.blade.php
+++ resources/views/classDocs.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documentazione classi</title>
-</head>
-<body style="margin:0; padding:0;">
-    <iframe src="{{ asset('docs\classes\index.html') }}" width="100%" height="1000px" style="border:none;"></iframe>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Documentazione classi</title>
+    </head>
+    <body style="margin:0; padding:0;">
+        <iframe src="{{ asset('docs\classes\index.html') }}" width="100%" height="1000px" style="border:none;"></iframe>
+    </body>
 </html>
